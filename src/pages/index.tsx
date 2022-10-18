5773e638cb234315e86a07c5d92f244632a1a2b0
damoondarvishi <damoon.darvishi@gmail.com>
2022-10-18 09:39:22 +0330

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="bg-indigo-500 h-screen text-3xl font-bold underline">
+      <p className="text-gray-700">Damoon</p>
+    </div>
+  )
+}
