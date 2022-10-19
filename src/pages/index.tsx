92af44807ad9073b4847a4e512b97ef0d8e70fe8
damoondarvishi <damoon.darvishi@gmail.com>
2022-10-19 22:56:40 +0330

    add project cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -227,11 +227,15 @@ export default function Home() {
                   </div>
                 </li>
               </ul>
+              <button>Show More</button>
             </section>
-            <section id='contact' className={'contact'}>
-              <h2>
+            <section id='contact' className='contact'>
+              <h2 className='numberd-heading'>
                 What’s Next?
               </h2>
+              <h2 className='title'>Get In Touch</h2>
+              <p>Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+              <a className='email-link' href='#'>Say Hello</a>
             </section>
           </main>
           <footer className={'footer'}>Footer</footer>
