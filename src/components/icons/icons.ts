4dad1f5e3622a9a2ec8b4d76068509851c3f43c2
damoon darvishi <damoon.darvishi@gmail.com>
2024-06-19 08:59:11 +0200

    generate icons components

---
+++ src/components/icons/icons.ts
@@ -0,0 +1,9 @@
+/* GENERATED FILE */
+
+export { default as IconCodepen } from './src/codepen';
+export { default as IconFolderIcon } from './src/folder-icon';
+export { default as IconGithub } from './src/github';
+export { default as IconInstagram } from './src/instagram';
+export { default as IconLinkIcon } from './src/link-icon';
+export { default as IconLinkedin } from './src/linkedin';
+export { default as IconTwitter } from './src/twitter';
---
+++ src/components/icons/src/linkedin.tsx
@@ -0,0 +1,10 @@
+import BaseIcon from "@/components/icons/base-icon";
+import { SvgIconType } from "@/components/icons/icon.types";
+
+export default function SvgIcon(props:SvgIconType) {
+  return (
+    <BaseIcon {...props}>
+      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect x="2" y="9" width="4" height="12"/><circle cx="4" cy="4" r="2"/>
+    </BaseIcon>
+  );
+}
