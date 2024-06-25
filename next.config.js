8a9a595f56ba5d0fe874922e117ceb327398c3df
damoon darvishi <damoon.darvishi@gmail.com>
2024-06-25 18:53:30 +0200

    config next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  basePath: isProd ? '/myPortfolio' : '',
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
