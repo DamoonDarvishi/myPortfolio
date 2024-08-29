5e33ad3bbb6e221f9f35f6c72bc738eeb92cd994
damoon darvishi <damoon.darvishi@gmail.com>
2024-08-29 21:53:57 +0200

    update and add motion

--- src/app/sections/herosection/HeroSection.tsx
+++ src/app/sections/herosection/HeroSection.tsx
@@ -1,11 +1,24 @@
+'use client'
 import React from 'react'
-import Link from 'next/link'
 import HandIcon from '@/assets/icons/hand-hi.svg'
+import Button from '@/components/button/Button'
+import { motion } from 'framer-motion'
 import './_hero.scss'
 
 export function HeroSection() {
   return (
-    <section className="hero">
+    <motion.div
+      className="hero"
+      id="hero"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+      variants={{
+        visible: { opacity: 1, y: -50 },
+        hidden: { opacity: 0, y: 0 },
+      }}
+    >
       <div className="hero_info">
         <div>
           <span className="say-hello">Hello</span>
@@ -13,15 +26,15 @@ export function HeroSection() {
         </div>
         <h2>I&apos;m Damoon</h2>
         <h3>Front-end Developer</h3>
-        <Link className="link-btn" href="#">
-          Check out my resume!
-        </Link>
+        <div className="contact-cta">
+          <Button text=" Check out my resume!" link="#" />
+        </div>
       </div>
       <div className="hero_photo">
         <div className="hero_photo-bg">
           <div className="hero_photo-bg-img"></div>
         </div>
       </div>
-    </section>
+    </motion.div>
   )
 }
