d74687380a1871c8283fcd9421e753fa689cb9d7
damoon darvishi <damon.darvishi@gmail.com>
2025-04-10 13:28:44 +0200

    update images paths

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,11 @@ import { type Metadata } from 'next'
 import Image from 'next/image'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoAnimaginary from '@/assets/logos/animaginary.svg'
+import logoCosmos from '@/assets/logos/cosmos.svg'
+import logoHelioStream from '@/assets/logos/helio-stream.svg'
+import logoOpenShuttle from '@/assets/logos/open-shuttle.svg'
+import logoPlanetaria from '@/assets/logos/planetaria.svg'
 
 const projects = [
   {
